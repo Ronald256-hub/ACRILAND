df284c6a0ea4df8110a6732be8baf9b75783b6ad
Ronald256-hub <ronaldecatu@gmail.com>
2026-08-15 16:26:11 +0300

    Load Phase 3 fleet health styles

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -6,4 +6,5 @@ import { AuthProvider } from "./auth/AuthContext";
 import "./styles.css";
 import "./styles-v2.css";
 import "./styles-v2-ops.css";
+import "./styles-v3.css";
 ReactDOM.createRoot(document.getElementById("root")!).render(<React.StrictMode><BrowserRouter><AuthProvider><App/></AuthProvider></BrowserRouter></React.StrictMode>);

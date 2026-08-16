47edd7a5f251dda34e3d2be9e06dc32c79fd9f92
Ronald256-hub <ronaldecatu@gmail.com>
2026-08-16 09:43:37 +0300

    Compose Vehicle 360 with integrated record hub

---
+++ apps/web/src/pages/Vehicle360IntegratedPage.tsx
@@ -0,0 +1,4 @@
+import { VehicleRecordHub } from "../components/VehicleRecordHub";
+import { Vehicle360Page } from "./Vehicle360Page";
+
+export function Vehicle360IntegratedPage(){return <><Vehicle360Page/><VehicleRecordHub/></>}

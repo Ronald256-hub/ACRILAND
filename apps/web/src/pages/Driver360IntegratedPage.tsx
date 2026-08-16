00a2f0e6c280468f1b994fdbcaf9ac3b5842ec9e
Ronald256-hub <ronaldecatu@gmail.com>
2026-08-16 09:43:46 +0300

    Compose Driver 360 with integrated record hub

---
+++ apps/web/src/pages/Driver360IntegratedPage.tsx
@@ -0,0 +1,4 @@
+import { DriverRecordHub } from "../components/DriverRecordHub";
+import { Driver360Page } from "./Driver360Page";
+
+export function Driver360IntegratedPage(){return <><Driver360Page/><DriverRecordHub/></>}

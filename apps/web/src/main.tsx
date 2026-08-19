5350623cdcb8a4c0a657a51c9e085517e8040d1f
Ronald256-hub <ronaldecatu@gmail.com>
2026-08-19 18:32:06 +0300

    Load Phase 8 and Phase 9 web styles

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -12,4 +12,6 @@ import "./styles-v5.css";
 import "./styles-v6.css";
 import "./styles-v6-secure.css";
 import "./styles-v7.css";
+import "./styles-v8.css";
+import "./styles-v9.css";
 ReactDOM.createRoot(document.getElementById("root")!).render(<React.StrictMode><BrowserRouter><AuthProvider><App/></AuthProvider></BrowserRouter></React.StrictMode>);
